cff596c8efda449e36310f54829e34ccc27dde3c
lancet <lancet@DESKTOP-RQ8R7DT>
2022-11-22 14:18:47 +0800

    --- automatic rebase to remote; old local SHA: 590aae7e06f16f2b77762929a57dfd32e6b49747 ---

--- src/application/ProtocolProcess.java
+++ src/application/ProtocolProcess.java
@@ -377,15 +377,15 @@ public class ProtocolProcess {
 	
 		else if (opType.equals("AddFrame")) {
 		  
-//		  tool.createFrame(bean.getParam().target, 
-//		      Transformation.ofRad(bean.getParam().getX(), bean.getParam().getY(), bean.getParam().getZ(), 
-//		          bean.getParam().getA(),bean.getParam().getB(), bean.getParam().getC()));
+		  tool.createFrame(bean.getParam().target, 
+		      Transformation.ofRad(bean.getParam().getX(), bean.getParam().getY(), bean.getParam().getZ(), 
+		          bean.getParam().getA(),bean.getParam().getB(), bean.getParam().getC()));
 		  
 		  log.info(bean.getParam().target);
 		  ProtocolResult ret = new ProtocolResult();
-//      ret.setOperateType("AddFrame");  
-//      ret.setResultMsg("Frame:"+bean.getParam().target);
-//      ret.setResultCode(0);
+      ret.setOperateType("AddFrame");  
+      ret.setResultMsg("Frame:"+bean.getParam().target);
+      ret.setResultCode(0);
       return ret;
 		}
 		
