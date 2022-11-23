5093697270e0ecae39281058a0be6d3d77cdc4c0
Administrator <Administrator@DESKTOP-OQEB286>
2022-11-23 15:27:10 +0800

    --- automatic rebase to remote; old local SHA: 502d7a329ac38fa06b54c4f47931c87fb66a5d1a ---

--- src/application/backgroudTest.java
+++ src/application/backgroudTest.java
@@ -80,11 +80,14 @@ public class backgroudTest extends RoboticsAPIApplication {
     info.forcetorque[5] = forceData.getTorque().getZ();
     
     
-    tool = (Tool)robot.findObject("tool");
-    
-    tool = (Tool)robot.findObject("tool");
+    try {
+      tool = (Tool)robot.findObject("tool");
+    } catch (Exception e) {
+      logger.info("cant find tool");
+      e.printStackTrace();
+    }
     if (tool == null) {
-      //no tool attached yet,return flange position
+      //no frame in tool added,return flange position
       Transformation trans = robot.getCurrentCartesianPosition(robot.getFlange()).getTransformationFromParent();
       trackingFrame f = new trackingFrame();
       f.position[0] = trans.getX();
